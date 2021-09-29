445f122567e45762b698ce824c10f5e39cf70677
beccaallen <becca@becca.work>
2021-09-29 17:02:01 -0500

    it works

--- app.js
+++ app.js
@@ -1,21 +1,38 @@
 let dieHolder = document.querySelector(".dieHolder");
 let newButton = document.querySelector(".newDie");
 let rollButton = document.querySelector(".roll");
+let sumButton = document.querySelector(".sum");
 let dieArray = [];
+let counter = 0;
 
 class Die {
   constructor() {
     this.div = document.createElement("div");
     this.div.className = "die";
-    this.value = this.roll();
+    this.value = document.createTextNode(counter);
     this.div.append(this.value);
+    this.roll();
     dieHolder.appendChild(this.div);
-    dieArray.push(this.div);
+    dieArray.push(this);
+    counter++;
+
+    //change value on die on click
+
+    this.div.addEventListener("click", () => this.roll());
+
+    //remove obj from div and global array on double click
+
+    this.div.addEventListener("dblclick", function (e) {
+      this.remove();
+      dieArray.splice(this, 1);
+      // check that correct obj was deleted from global array
+      // console.log(dieArray)
+    });
   }
 
   roll() {
-    let randomVal = Math.floor(Math.random() * 7);
-    return `${randomVal}`;
+    this.value = Math.floor(Math.random() * 7);
+    this.div.textContent = this.value;
   }
 }
 
@@ -23,11 +40,14 @@ newButton.addEventListener("click", function () {
   new Die();
 });
 
+rollButton.addEventListener("click", function () {
+  for (i = 0; i < dieArray.length; i++) {
+    dieArray[i].roll();
+  }
+});
 
-rollButton.addEventListener("click", function (){
-    for (i=0; i < dieArray.length; i++){
-        dieArray[i]
-        console.log(dieArray[i]);
-    }
-   
-})
+sumButton.addEventListener("click", function () {
+  let val = dieArray.map((a) => a.value);
+  const reducer = (previousValue, currentValue) => previousValue + currentValue;
+  alert(val.reduce(reducer));
+});
